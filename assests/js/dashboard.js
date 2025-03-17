5833f09fd3230b68a2d5a09daab02574e035a58d
EllyEllens <150268047+EllyEllens@users.noreply.github.com>
2025-03-17 10:34:27 -0300

    Delete assests directory

--- assests/js/dashboard.js
+++
@@ -1,8 +0,0 @@
-// Sidebar toggle functionaliteit
-const sidebar = document.getElementById('sidebar');
-const sidebarToggle = document.getElementById('sidebar-toggle');
-
-sidebarToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('open');
-    document.querySelector('.main-content').style.marginLeft = sidebar.classList.contains('open') ? '250px' : '0';
-});
